d15397de7f4305ab4cd4cce365f6cbddc3c40cd7
Faizul Haque <haquefaizul92@gmail.com>
2023-11-22 08:16:12 +0530

    Spring boot project pushed successfully

---
+++ src/main/java/com/crud/SpringBootCrudProject6464Application.java
@@ -0,0 +1,11 @@
+package com.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudProject6464Application {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudProject6464Application.class, args);
+	}
+}
---
+++ src/main/java/com/crud/service/CustomerService.java
@@ -0,0 +1,51 @@
+package com.crud.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.crud.entity.Customer;
+import com.crud.repo.CustomerRepository;
+
+@Service
+
+public class CustomerService {
+	@Autowired
+	private CustomerRepository customerRepository;
+	
+	
+	public Customer postCustomer(Customer customer)
+	{
+		return customerRepository.save(customer);
+	}
+	
+	public List<Customer> getAllCustomer()
+	{
+		return customerRepository.findAll();
+	}
+	
+	public void deleteCustomer(Long id)
+	{
+		customerRepository.deleteById(id);
+	}
+	
+	public Customer getCustomerById(Long id)
+	{
+		return customerRepository.findById(id).orElse(null);
+	} 
+	public Customer updateCustomer(Long id, Customer customer)
+	{
+		Optional<Customer> optionalCustomer=customerRepository.findById(id);
+		if(optionalCustomer.isPresent())
+		{
+			Customer existingCustomer=optionalCustomer.get();
+			existingCustomer.setEmail(customer.getEmail());
+			existingCustomer.setName(customer.getName());
+			existingCustomer.setPhone(customer.getPhone());
+			return customerRepository.save(existingCustomer);
+		}
+		return null;
+	}
+}
